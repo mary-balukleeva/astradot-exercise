b259ee1c017ff313776e137bd05f132b0d788cda
Mary Balukleeva <mary.balkleeva10@gmail.com>
2020-07-15 16:10:23 +0300

    Updates

---
+++ precache-manifest.31998d2ea8e5d5ae7c3203072b43f665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1efbca8107ba79f3daaaf78d44afab",
+    "url": "/astradot-exercise/index.html"
+  },
+  {
+    "revision": "6113fe693bc451eaf6d5",
+    "url": "/astradot-exercise/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad72ada6ed6b7009db93",
+    "url": "/astradot-exercise/static/js/2.c0e47894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/astradot-exercise/static/js/2.c0e47894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6113fe693bc451eaf6d5",
+    "url": "/astradot-exercise/static/js/main.7bc29401.chunk.js"
+  },
+  {
+    "revision": "7dc329a7f6ccfc888db7",
+    "url": "/astradot-exercise/static/js/runtime-main.9631a315.js"
+  }
+]);
